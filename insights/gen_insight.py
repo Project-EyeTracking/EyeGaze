aaae2b4cfacdb0978d7f98431b0699f51c957cb9
BlessonM <blessonma6@gmail.com>
2025-01-27 19:25:47 +0100

    run on the data from the screen_translation module
    few changes

--- insights/gen_insight.py
+++ insights/gen_insight.py
@@ -16,15 +16,16 @@ def main():
     x_divisions = 2
     y_divisions = 2
 
-    instruction_df = pd.read_csv("D:\MAI\THWS\project\EyeGaze\insights\inst.csv")    #need to be configured later
-    observation_df = pd.read_csv("D:\MAI\THWS\project\EyeGaze\insights\obs_2.csv")
+    instruction_df = pd.read_csv("D:\MAI\THWS\project\EyeGaze\insights\data\inst.csv")    # need to be configured later
+    observation_df = pd.read_csv("D:\MAI\THWS\project\EyeGaze\insights\data\obs.csv")
+
 
     #print(instruction_df)
     #print('\n')
     #print(observation_df)
 
-    time_levels=3    #vgoof, good, ok, 
-    time_window = 3  #time_period of  a time level
+    time_levels=3    # vgoof, good, ok, 
+    time_window = 3  # time_period of  a time level,  if the    
     time_out = time_levels*time_window
 
     latency = list()
@@ -33,7 +34,7 @@ def main():
     hit_y = list()
     hit_time = list()
 
-    accuracy_threshold = 1600
+    accuracy_threshold = 1600  # maximum accuracy is 2000 that is if the isntruction coordinates are followed exactly
 
     for i in range(len(instruction_df['time'])):
         for j in range(i,len(observation_df['time'])):
@@ -52,27 +53,30 @@ def main():
             y2 = observation_df['y_cordinate'][j]
             y = [y1,y2]
             
-            accuracy = 2000 - distance(x,y)
-
+            accuracy = 2000 - distance(x,y)  # maximum accuracy is defined to be 2000
             if(accuracy>accuracy_threshold):
                     hit_accuracy.append(accuracy)
                     latency.append(t2-t1)
                     hit_x.append(x2)
                     hit_y.append(y2)
                     hit_time.append(t2)
-
                     break
     avg_accuracy = sum(hit_accuracy)/len(instruction_df['time'])
-    #print(hit_accuracy)
-    #print(avg_accuracy)    
+    print(f"hit_accuracy: {hit_accuracy}")
+    print(f"avg_accuracy: {avg_accuracy}")    
 
     avg_latency = sum(latency)/len(instruction_df['time'])
-    #print(latency)
-    #print(avg_latency)    
-    #print(hit_x,hit_y)   
-    #print(hit_time) 
+    print(f"latency: {latency}")
+    print(f"avg_latency: {avg_latency}")    
+    print(f"{hit_x},{hit_y}")   
+    print(f"{hit_time}") 
 
     return hit_accuracy, latency, hit_time, hit_x, hit_y      
 
+          
+          
+          
+          
 if __name__ == "__main__":
-    main()
+
+    hit_accuracy, latency, hit_time, hit_x, hit_y =  main()
